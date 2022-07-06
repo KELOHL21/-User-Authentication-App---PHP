7e5c6770883ea0e313bc4bbf4765ec791848b5a5
KELOHL21 <kelsyohlsson@gmail.com>
2022-07-06 11:09:42 +0200

    Update

--- members_page.php
+++ members_page.php
@@ -17,7 +17,7 @@ if (!isset( $_SESSION['member_name'])) {
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Labyrinth Library Librarian Home</title>
+   <title>Labyrinth Library Home</title>
    <link rel="stylesheet" href="css/stylesheet.css">
 </head>
 <body>
